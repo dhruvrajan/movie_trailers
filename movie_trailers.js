550dd818837c1cd27c4ea209c4f0fa29baf1c060
Dhruv Rajan <dhruv@garuda.local>
2015-06-15 22:23:13 -0700

    Concise youtube api

--- movie_trailers.js
+++ movie_trailers.js
@@ -23,6 +23,8 @@ if (Meteor.isClient) {
         },
 
         'submit .add-by-search': function(event) {
+            event.preventDefault();
+
             var title = event.target.title.value;
             //var movie_data = Meteor.call("searchMovie", title);
             //Meteor.call("addMovie", title, title, title, title);
@@ -49,72 +51,75 @@ if (Meteor.isClient) {
     });
 }
 
-Meteor.methods({
-    addMovie: function(title, description, poster_url, youtube_url) {
-        var trailer_youtube_id = youtube_url.substring(youtube_url.indexOf("watch?v=") + 8, youtube_url.length);
-        Movies.insert({
-            title: title,
-            description: description,
-            poster_url: poster_url,
-            youtube_url: youtube_url,
-            trailer_youtube_id: trailer_youtube_id
-        });
-    },
-
-    searchVideo: function(search) {
-        YoutubeApi.search.list({
-            part: "id",
-            type: "video",
-            maxResults: 5,
-            q: search,
-        }, function(err, data) {
-            console.log(err, data);
-           });
-    },
-
-    deleteMovie: function(id) {
-        Movies.remove(id);
-    },
-
-    searchMovie: function(title) {
-        var result = Meteor.wrapAsync(HTTP.get)("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json");
-        console.log("@result: ", result);
-        if (result && result.data.Error) {
-            console.log("@raised: ", result.data.Error);
-            throw new Meteor.Error(result.data.Error);
-        }
-        var result2 = Meteor.wrapAsync(YoutubeApi.search.list.bind(YoutubeApi.search))({
-            part: "id",
-            type: "video",
-            maxResults: 3,
-            q: result.data.Title + " trailer"
-        });
-
-        Meteor.call("addMovie", result.data.Title, result.data.Plot, result.data.Poster, "https://www.youtube.com/watch?v="+result2.items[0].id.videoId);
-
-        console.log("@omdbapi: ", result.data.Title, ", plot: ", result.data.Plot, ", ID: ", result2.items[0].id.videoId);
-
-        //var list = Meteor.wrapAsync(YoutubeApi.search.list.bind(YoutubeApi.search));
-        //get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json", function(error, result) {
-        //    if (!error) {
-        //        console.log(result.data.Plot);
-        //        list({
-        //            part: "id",
-        //            type: "video",
-        //            maxResults: 3,
-        //            q: result.data.Title + " trailer"
-        //        }, function(error2, result2) {
-        //            if (!error2) {
-        //                console.log(typeof(result2.items[0].id.videoId));
-        //                Meteor.call("addMovie", result.data.Title, result.data.Plot, result.data.Poster, "https://www.youtube.com/watch?v="+result2.items[0].id.videoId);
-        //            }
-        //        });
-        //    }
-        //});
-    }
-});
-
 if (Meteor.isServer) {
+
+    Meteor.methods({
+        addMovie: function(title, description, poster_url, youtube_url) {
+            var trailer_youtube_id = youtube_url.substring(youtube_url.indexOf("watch?v=") + 8, youtube_url.length);
+            Movies.insert({
+                title: title,
+                description: description,
+                poster_url: poster_url,
+                youtube_url: youtube_url,
+                trailer_youtube_id: trailer_youtube_id
+            });
+        },
+
+        searchVideo: function(search) {
+            YoutubeApi.search.list({
+                part: "id",
+                type: "video",
+                maxResults: 5,
+                q: search,
+            }, function(err, data) {
+                console.log(err, data);
+            });
+        },
+
+        deleteMovie: function(id) {
+            Movies.remove(id);
+        },
+
+        searchMovie: function(title) {
+            var result = Meteor.wrapAsync(HTTP.get)("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json");
+
+            if (result && result.data.Error) {
+                console.log("@raised: ", result.data.Error);
+                throw new Meteor.Error(result.data.Error);
+            }
+
+            console.log("@on to youtube");
+            var result2 = Meteor.wrapAsync(YoutubeApi.search.list.bind(YoutubeApi.search))({
+                part: "id",
+                type: "video",
+                maxResults: 3,
+                q: result.data.Title + " trailer"
+            });
+
+            Meteor.call("addMovie", result.data.Title, result.data.Plot, result.data.Poster, "https://www.youtube.com/watch?v="+result2.items[0].id.videoId);
+
+            console.log("@omdbapi: ", result.data.Title, ", plot: ", result.data.Plot, ", ID: ", result2.items[0].id.videoId);
+
+            //var list = Meteor.wrapAsync(YoutubeApi.search.list.bind(YoutubeApi.search));
+            //get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&r=json", function(error, result) {
+            //    if (!error) {
+            //        console.log(result.data.Plot);
+            //        list({
+            //            part: "id",
+            //            type: "video",
+            //            maxResults: 3,
+            //            q: result.data.Title + " trailer"
+            //        }, function(error2, result2) {
+            //            if (!error2) {
+            //                console.log(typeof(result2.items[0].id.videoId));
+            //                Meteor.call("addMovie", result.data.Title, result.data.Plot, result.data.Poster, "https://www.youtube.com/watch?v="+result2.items[0].id.videoId);
+            //            }
+            //        });
+            //    }
+            //});
+        }
+    });
+
     Meteor.startup(function() {
         // code to run on server at startup
         YoutubeApi.authenticate({
