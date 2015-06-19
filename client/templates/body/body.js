b9b1f719aac782fb4095040543ca3ea6d8c93b3d
Dhruv Rajan <dhruv@garuda.local>
2015-06-19 10:15:00 -0700

    Added Polymer paper-header

--- client/templates/body/body.js
+++ client/templates/body/body.js
@@ -2,15 +2,15 @@ Template.body.helpers({
   //movies: function() {
   //    return Movies.find();
   //},
-  user_movies: function() {
+  user_movies: function () {
     var sort_query = {};
-    sort_query[Session.get("sort-by")] =  -1;
+    sort_query[Session.get("sort-by")] = -1;
     return Movies.find({owner: Meteor.userId()}, {sort: sort_query})
   }
 });
 
 Template.body.events({
-  'submit .new-movie': function(event) {
+  'submit .new-movie': function (event) {
     var title = event.target.title.value;
     var description = event.target.description.value;
     var poster_url = event.target.poster_url.value;
@@ -18,12 +18,12 @@ Template.body.events({
     Meteor.call("addMovie", title, description, poster_url, youtube_url)
   },
 
-  'change .order-by': function(event) {
+  'change .order-by': function (event) {
     //console.log("@change sort: ", event.target.value);
     Session.set("sort-by", event.target.value)
   },
 
-  'submit .add-by-search': function(event) {
+  'submit .add-by-search': function (event) {
     event.preventDefault();
 
     var title = event.target.title.value;
